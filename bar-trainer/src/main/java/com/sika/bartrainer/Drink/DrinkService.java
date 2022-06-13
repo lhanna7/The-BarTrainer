b20ee08f8429098f4f040ed1f0ba0ca6c65f1a2d
Lauren <lhanna7@ford.com>
2022-06-13 12:45:47 -0700

    finish db work

--- bar-trainer/src/main/java/com/sika/bartrainer/Drink/DrinkService.java
+++ bar-trainer/src/main/java/com/sika/bartrainer/Drink/DrinkService.java
@@ -23,14 +23,33 @@ public class DrinkService {
   }
 
   public Optional<Drink> update(Drink drink) {
-    Optional<Drink> foundResource = drinkRepository.findById(drink.getId());
+    Optional<Drink> foundDrink = drinkRepository.findById(drink.getId());
 
-    if (foundResource.isPresent()) {
-      Drink updatedResource = foundResource.get();
-      updatedResource.setSomeProperty(drink.getSomeProperty());
+    if (foundDrink.isPresent()) {
+      Drink updatedDrink = foundDrink.get();
+      updatedDrink.setId(drink.getId());
+      updatedDrink.setName(drink.getName());
+      updatedDrink.setGlass(drink.getName());
+      updatedDrink.setMethod(drink.getMethod());
+      updatedDrink.setIngredient1(drink.getIngredient1());
+      updatedDrink.setIngredient2(drink.getIngredient2());
+      updatedDrink.setIngredient3(drink.getIngredient3());
+      updatedDrink.setIngredient4(drink.getIngredient4());
+      updatedDrink.setIngredient5(drink.getIngredient5());
+      updatedDrink.setIngredient6(drink.getIngredient6());
+      updatedDrink.setTopper(drink.getTopper());
+      updatedDrink.setInstruction1(drink.getInstruction1());
+      updatedDrink.setInstruction2(drink.getInstruction2());
+      updatedDrink.setInstruction3(drink.getInstruction3());
+      updatedDrink.setInstruction4(drink.getInstruction4());
+      updatedDrink.setInstruction5(drink.getInstruction5());
+      updatedDrink.setInstruction6(drink.getInstruction6());
+      updatedDrink.setInstruction7(drink.getInstruction7());
+      updatedDrink.setGarnish(drink.getGarnish());
+      updatedDrink.setImageurl(drink.getImageurl());
 
-      drinkRepository.save(updatedResource);
-      return Optional.of(updatedResource);
+      drinkRepository.save(updatedDrink);
+      return Optional.of(updatedDrink);
     } else {
       return Optional.empty();
     }
